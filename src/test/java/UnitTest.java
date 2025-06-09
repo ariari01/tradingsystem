4849bde39d9a77286d1af1b42a2ed398b6b3696f
Wonchul Lee <wonchul08.lee@gmail.com>
2025-06-09 09:51:28 +0900

    2

--- src/test/java/UnitTest.java
+++ src/test/java/UnitTest.java
@@ -1,8 +1,24 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 public class UnitTest {
 
+    @Test
+    void 키워를_선택하면_KiwerAPI를_반환한다() {
+        Object o = StockSelector.selectStockBrocker("kiwer");
+
+        assertThat(o).isInstanceOf(KiwerAPI.class);
+    }
+
+    @Test
+    void Nemo를_선택하면_NemoAPI를_반환한다() {
+        Object o = StockSelector.selectStockBrocker("Nemo");
+
+        assertThat(o).isInstanceOf(KiwerAPI.class);
+    }
 }
