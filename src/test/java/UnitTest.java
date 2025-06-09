c445482353bb44c2d84570d7db9d3b064c5994c8
Wonchul Lee <wonchul08.lee@gmail.com>
2025-06-09 09:31:54 +0900

    first

---
+++ src/test/java/UnitTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UnitTest {
+
+}
